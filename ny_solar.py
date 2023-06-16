c208338f1143c972a66657b8c0e62e5ee9966189
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-06-16 15:17:15 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 import streamlit as st
 
-import plotly as pt
-
 data = pd.read_csv("/Users/anthony/Desktop/NY_SOLAR.csv")
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
