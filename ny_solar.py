6a372bc1ce9b3079b79e803ae178022bcd1583eb
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-06-16 15:31:35 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -8,4 +8,5 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 st.markdown('Metrics')
 col1, col2, col3 = st.columns(3)
-col1.metric("The numbe of projects", data['Project ID'].count())
+col1.metric("The number of projects", data['Project ID'].count())
+col2.metric("Most popular area", data['Zip'].value_counts())
