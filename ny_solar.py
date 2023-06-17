5cb1818303befd625986e65d2fb7db1875d73ea1
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-06-17 18:49:46 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -12,10 +12,13 @@ value_counts = data['Zip'].value_counts()
 # Find the value that repeats the most number of times
 most_common_value = value_counts.idxmax()
 
+# Sum of the annual PV energy production (GWh)
+sum_annual_prod = (data['Estimated Annual PV Energy Production (kWh)'].sum())) / 10**6
+
 
 st.markdown('Metrics')
 col1, col2, col3 = st.columns(3)
 col1.metric("The number of projects", data['Project ID'].count())
 col2.metric("Most popular area", int(most_common_value))
-col3.metric("test", data['Estimated Annual PV Energy Production (kWh)'].sum())
+col3.metric("Total Annual Production (GWH)", sum_annual_prod)
 
