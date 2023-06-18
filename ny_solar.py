8f2c21495d42b4454827affdc84c5d08b97da31d
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-06-18 08:54:00 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -27,3 +27,5 @@ col1, col2, col3 = st.columns(3)
 col1.metric("The number of projects", data['Project ID'].count())
 col2.metric("Zip code with the most number of installations", int(most_common_value))
 col3.metric("Estimated annual production (GWh)", round(conversion))
+
+st.bar_chart(data['Utility'])
