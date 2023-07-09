1df66841113049e31408025c76c9d9d73ac5e77a
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-07-09 17:06:45 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -28,7 +28,7 @@ subsetoooor = data[data['County'] == selected_county][['County', 'Estimated Annu
 
 sum_pv_system_size = subsetoooor['Estimated Annual PV Energy Production (kWh)'].sum()
 
-convert = sum_pv_system_size / 10 ** 6
+convert = int(sum_pv_system_size) / 10 ** 6
 
 st.markdown('Metrics')
 col1, col2, col3 = st.columns(3)
