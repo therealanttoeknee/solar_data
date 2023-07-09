7e01dcb3e101cb4e8e2f1145eb451c33cc29d43a
therealanttoeknee <127083762+therealanttoeknee@users.noreply.github.com>
2023-07-09 16:52:51 -0400

    Update ny_solar.py

--- ny_solar.py
+++ ny_solar.py
@@ -13,7 +13,6 @@ unique_counties = pd.DataFrame(data['County'].unique(), columns = ['unique_count
 #Reorganizes the column in alphabetical order
 unique_counties = unique_counties.sort_values(by='unique_county', ascending=True)
 
-
 #sidebar
 st.sidebar.header("Welcome! :-) ")
 
@@ -24,20 +23,13 @@ with st.sidebar:
 # Count the occurrences of each unique value in the column for that specific county
 value_counts = data[data['County'] == selected_county]['County'].value_counts()
 
-filter = data[data['Estimated Annual PV Energy Production (kWh)'] == selected_county]['Estimated Annual PV Energy Production (kWh)'].value_counts()
-
-# Remove commas from values and convert to numeric type
-#data['Estimated Annual PV Energy Production (kWh)'] = pd.to_numeric(data['Estimated Annual PV Energy Production (kWh)'].str.replace(',', ''), errors='coerce')                                                                    
-
-# Calculate the sum of the values
-#total_sum = data['Estimated Annual PV Energy Production (kWh)'].sum()
-
-#conversion = total_sum / 10 ** 6
+# subset the data frame to select "selected_county" and each kW-DC value 
+subsetoooor = data[['County', 'Utility']]
 
 st.markdown('Metrics')
 col1, col2, col3 = st.columns(3)
 col1.metric("The number of projects", value_counts)
-col2.metric("Estimated Annual Production", filter.astype(int))
+col2.metric("Estimated Annual Production", filter)
 
 
 
